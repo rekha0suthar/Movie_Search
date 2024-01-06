779b45623320a89e4066965f329def8099452897
rekha0suthar <rekha0suthar@gmail.com>
2024-01-06 14:12:51 +0530

    application build done

---
+++ movie-search-rekha/build/precache-manifest.2addb70d4631c25cd262e175cfc591f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d4074cc15298cc988d0829792b059c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7519182018d342ada675",
+    "url": "/static/css/main.27be6563.chunk.css"
+  },
+  {
+    "revision": "ffc62b52d0a41f5417e5",
+    "url": "/static/js/2.b38a5980.chunk.js"
+  },
+  {
+    "revision": "f6c7e36f6b16f2f7f02269e692bf2d9c",
+    "url": "/static/js/2.b38a5980.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7519182018d342ada675",
+    "url": "/static/js/main.46d80965.chunk.js"
+  },
+  {
+    "revision": "9f97e5952bc8b13ef682",
+    "url": "/static/js/runtime-main.e65ce676.js"
+  }
+]);
